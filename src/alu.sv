6f5687706cf2d12ff3a4799915c5f4e60d66e513
arhamhashmi01 <arhamhashmi82@gmail.com>
2024-04-10 10:56:31 +0500

    componentes

---
+++ src/alu.sv
@@ -0,0 +1,50 @@
+module alu #(
+    parameter DataWidth = 32;
+    )
+    (
+        input logic [DataWidth-1 : 0] operand_a,
+        input logic [DataWidth-1 : 0] operand_b,
+        input logic [3 : 0] alu_control,
+
+        output logic [DataWidth-1 : 0] result
+    );
+
+    always_comb begin
+        if (alu_control == 4'b0000) begin
+            result = operand_a + operand_b;  //add
+        end
+        else if (alu_control == 4'b0001) begin
+            result = operand_a - operand_b;  //sub
+        end
+        else if (alu_control == 4'b0010) begin
+            result = operand_a << operand_b; //shift left logical
+        end
+        else if (alu_control == 4'b0011) begin
+            result = $signed (operand_a) < $signed (operand_b); //shift less then
+        end 
+        else if (alu_control == 4'b0100) begin
+            result = operand_a < operand_b;  //shift less then unsigned
+        end          
+        else if (alu_control == 4'b0101) begin
+            result = operand_a ^ operand_b;  //xor
+        end
+        else if (alu_control == 4'b0110) begin
+            result = operand_a >> operand_b; //shift right logical
+        end
+        else if (alu_control == 4'b0111) begin
+            result = operand_a >>> operand_b; //shift right arithematic
+        end
+        else if (alu_control == 4'b1000) begin
+            result = operand_a | operand_b;   //or
+        end
+        else if (alu_control == 4'b1001) begin
+            result = operand_a & operand_b;  //and
+        end
+        else if (alu_control == 4'b1111) begin
+            result = operand_b;               //for lui 
+        end
+        else begin
+            result = 0;
+        end
+    end
+endmodule
---
+++ src/register_file.sv
@@ -0,0 +1,38 @@
+module register_file #(
+    parameter DataWidth  = 32;
+    parameter RegAddress = 5;
+    )
+    (
+        input logic clk,
+        input logic rst,
+        input logic write_enale,
+        input logic [RegAddress-1 : 0] source1,
+        input logic [RegAddress-1 : 0] source2,
+        input logic [RegAddress-1 : 0] writedata_add,
+        input logic [DataWidth-1  : 0] write_data,
+
+        output logic [DataWidth-1 : 0] readdata1,
+        output logic [DataWidth-1 : 0] readdata2
+    );
+
+    localparam number_of_registers = 32;
+
+    logic [DataWidth-1 : 0] RegFile [number_of_registers-1 : 0];
+
+    integer i;
+    always_ff @( posedge clk or negedge rst ) begin
+        if (!rst) begin
+            for (i=0 ; i < number_of_registers ; i++)
+                RegFile <= 0;
+        end
+        else if (write_enale) begin
+            if (writedata_add = 5'd0)
+                RegFile[0] <= 32'h00000000;
+            else
+                RegFile[writedata_add] <= write_data
+        end
+    end
+
+    assign readdata1 = RegFile[source1];
+    assign readdata1 = RegFile[source2];
+endmodule
