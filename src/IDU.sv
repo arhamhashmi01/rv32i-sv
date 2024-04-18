119f49b370229791e58a617fb1b0d00c91e4d68e
arhamhashmi01 <arhamhashmi82@gmail.com>
2024-04-19 00:51:42 +0500

    decode unit

--- src/IDU.sv
+++ src/IDU.sv
@@ -26,10 +26,15 @@ module decode #(
     output logic [DataWidth-1 : 0] opb_mux_out
     );
 
+    logic func3;
+    logic func7;
+    logic opcode;
     logic branch;
     logic operand_a;
     logic operand_b;
     logic [2:0]  imm_sel;
+    logic [RegAddress-1: 0] rs_1;
+    logic [RegAddress-1: 0] rs_2;
     logic [DataWidth-1 : 0] op_a;
     logic [DataWidth-1 : 0] op_b;
     logic [DataWidth-1 : 0] i_immo;
@@ -39,12 +44,19 @@ module decode #(
     logic [DataWidth-1 : 0] u_immo;
     logic [DataWidth-1 : 0] imm_mux_out;
 
+    assign fun7 = instruction[30];
+    assign fun3 = instruction[14:12];
+    assign rs_1 = instruction[19:15];
+    assign rs_1 = instruction[24:20];
+    assign rd = instruction_rd[11:7];
+    assign opcode = instruction[6:0];
+
     // CONTROL UNIT
     controlunit u_cu0 
     (
-        .opcode(instruction[6:0]),
-        .fun3(instruction[14:12]),
-        .fun7(instruction[30]),
+        .opcode(opcode),
+        .fun3(func3),
+        .fun7(func7),
         .valid(valid),
         .reg_write(reg_write_en_out),
         .imm_sel(imm_sel),
@@ -89,13 +101,13 @@ module decode #(
     (
         .clk(clk),
         .rst(rst),
-        .en(reg_write_en_in),
-        .rs1(instruction[19:15]),
-        .rs2(instruction[24:20]),
-        .rd(instruction_rd[11:7]),
-        .data(rd_wb_data),
-        .op_a(op_a),
-        .op_b(op_b)
+        .write_enale(reg_write_en_in),
+        .source1(rs_1),
+        .source2(rs_2),
+        .writedata_add(rd),
+        .write_data(rd_wb_data),
+        .readdata1(op_a),
+        .readdata2(op_b)
     );
 
     assign rs1 = instruction[19:15];
@@ -116,8 +128,8 @@ module decode #(
     )u_branch0
     (
         .en(branch),
-        .op_a(op_a),
-        .op_b(op_b),
+        .operand_a(op_a),
+        .operand_b(op_b),
         .fun3(instruction[14:12]),
         .result(branch_result)
     );
