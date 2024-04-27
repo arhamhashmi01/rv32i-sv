03e838edf9dd1fdbd250c6cf34c4849513becb3b
Hasan Ahmed Abbasi <99488337+Hasan0712@users.noreply.github.com>
2024-04-27 20:00:03 +0500

    Processor

--- src/Core.sv
+++ src/Core.sv
@@ -21,6 +21,28 @@ module core #(
     output logic [DataWidth-1 : 0] pc_address
 );
     
+    logic load_decode;
+    logic jalr_decode;
+    logic store_decode;
+    logic reg_write_wb;
+    logic branch_decode;
+    logic next_sel_decode;
+    logic reg_write_decode;
+    logic branch_result_decode;
+    logic [3 : 0] mask;
+    logic [1 : 0] mem_to_reg_decode;
+    logic [3 : 0] alu_control_decode;
+    logic [DataWidth-1 : 0] rd_wb_data;
+    logic [DataWidth-1 : 0] op_b_decode;
+    logic [DataWidth-1 : 0] instruction_wb;
+    logic [DataWidth-1 : 0] pc_address_fetch;
+    logic [DataWidth-1 : 0] instruction_fetch;
+    logic [DataWidth-1 : 0] opa_mux_out_decode;
+    logic [DataWidth-1 : 0] opb_mux_out_decode;
+    logic [DataWidth-1 : 0] wrap_load_memstage;
+    logic [DataWidth-1 : 0] alu_res_out_execute;
+    logic [DataWidth-1 : 0] next_sel_address_execute;
+
 
     //FETCH STAGE
     fetch #(
@@ -30,10 +52,10 @@ module core #(
         .clk(clk),
         .rst(rst),
         .load(load_decode),
-        .jalr(jalr_execute),
-        .branch(branch),
-        .next_sel(next_sel_execute),
-        .branch_reselt(branch_result_execute),
+        .jalr(jalr_decode),
+        .branch(branch_decode),
+        .next_sel(next_sel_decode),
+        .branch_reselt(branch_result_decode),
         .next_address(alu_res_out_execute),
         .instruction_fetch(instruction),
         .instruction(instruction_fetch),
@@ -42,7 +64,7 @@ module core #(
         .mask(instruc_mask_singal),
         .we_re(instruction_mem_we_re),
         .request(instruction_mem_request),
-        .pre_address_pc(pre_pc_addr_fetch),
+        .pre_address_pc(pc_address_fetch),
         .pc_address(pc_address)
     );
 
@@ -57,15 +79,15 @@ module core #(
         .valid(data_mem_valid),
         .load_control_signal(load_execute),
         .reg_write_en_in(reg_write_wb),
-        .instruction(instruction_decode),
-        .pc_address(pre_pc_addr_decode),
+        .instruction(instruction_fetch),
+        .pc_address(pc_address_fetch),
         .rd_wb_data(rd_wb_data),
         .rs1(rs1_decode),
         .rs2(rs2_decode),
         .load(load_decode),
         .store(store_decode),
         .jalr(jalr_decode),
-        .Branch(branch),
+        .Branch(branch_decode),
         .next_sel(next_sel_decode),
         .reg_write_en_out(reg_write_decode),
         .mem_to_reg(mem_to_reg_decode),
@@ -82,13 +104,47 @@ module core #(
         .DataWidth(DataWidth)
     )u_executestage
     (
-        .operand_a(alu_in_a),
-        .operand_b(alu_in_b),
-        .pc_address(pre_pc_addr_execute),
-        .alu_control(alu_control_execute),
+        .operand_a(opa_mux_out_decode),
+        .operand_b(opb_mux_out_decode),
+        .pc_address(pc_address_fetch),
+        .alu_control(alu_control_decode),
         .alu_out(alu_res_out_execute),
         .next_sel_address(next_sel_address_execute)
     );
 
+    //MEMORY STAGE
+    memory_stage #(
+        .DataWidth(DataWidth)
+    )u_memorystage
+    (
+        .rst(rst),
+        .load(load_decode),
+        .store(store_decode),
+        .operand_b(op_b_decode),
+        .instruction(instruction_fetch),
+        .alu_out_address(alu_res_out_execute),
+        .wrap_load_in(load_data_in),
+        .mask(mask),
+        .data_valid(data_mem_valid),
+        .valid(instruc_mem_valid),
+        .we_re(data_mem_we_re),
+        .request(data_mem_request),
+        .store_data_out(store_data_out),
+        .wrap_load_out(wrap_load_memstage)
+    );
 
+    assign reg_write_wb   = reg_write_decode;
+    assign instruction_wb = instruction_fetch;
+
+    //WRITE BACK STAGE
+    write_back #(
+        .DataWidth(DataWidth)
+    )u_wbstage
+    (
+        .mem_to_reg(mem_to_reg_decode),
+        .alu_out(alu_res_out_execute),
+        .data_mem_out(wrap_load_memstage),
+        .next_sel_address(next_sel_address_execute),
+        .rd_mux_out(rd_wb_data)
+    );
 endmodule
